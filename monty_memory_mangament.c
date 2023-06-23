7b871479c2b748704ec2d8b9b4c094fd42d1edc2
noguia <noguiaisback@gmail.com>
2023-06-22 18:28:35 -0700

    memory managment

---
+++ monty_memory_mangament.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/** bu noguiaa & norajeout */
+
+/**
+ * free_info - free data
+ *
+ *
+ */
+void free_info(void)
+{
+	free(info.cmd);
+	info.cmd = NULL;
+	free(info.arg);
+	info.arg = NULL;
+	fclose(info.fp);
+}
+/**
+ * free_list - free list
+ * @stack: stack or queue
+ */
+void free_list(stack_t *stack)
+{
+	stack_t *node;
+
+	while (stack)
+	{
+		node = stack;
+		stack = stack->next;
+		free(node);
+	}
+}
