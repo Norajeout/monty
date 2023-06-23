0e9ef5de916d26cb6d6da3f9ae0100c7abbe640b
noguia <noguiaisback@gmail.com>
2023-06-22 18:29:32 -0700

    monty

---
+++ monty.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/** by noguiaa & norajeout */
+
+/**
+ * main - an interpreter for Monty ByteCodes files.
+ * @argc: Argument Count
+ * @argv: Argument Value
+ * Return: Exit_Succes(0) or Exit_Failure(1)
+ */
+int main(int argc, char **argv)
+{
+	info.type = LIFO;
+	if (argc != 2)
+		handle_error(2);
+	else
+	{
+	treat_monty(argv[1]);
+	}
+	return (EXIT_SUCCESS);
+}
